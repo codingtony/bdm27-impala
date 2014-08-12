8f4fc3fcca66e683f36509f4d945d966ff1e584c
Tony Bussieres <tony.bussieres@ticksmith.com>
2014-08-11 20:13:19 -0400

    fix year for high/low queries

--- sql/hitsparquet.sql
+++ sql/hitsparquet.sql
@@ -9,12 +9,12 @@ explain select count(day) from hitsparquet  where Year = 2013;
 select count(day) from hitsparquet  where Year = 2013;
 
 -- High/Low IBM 1 month
-explain select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2014 AND MONTH=07  GROUP by day,ticker ORDER by day;
-select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2014 AND MONTH=07  GROUP by day,ticker ORDER by day;
+explain select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2013 AND MONTH=07  GROUP by day,ticker ORDER by day;
+select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2013 AND MONTH=07  GROUP by day,ticker ORDER by day;
 
 -- High/Low IBM 1 year
-explain select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2014  GROUP by day,ticker ORDER by day;
-select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2014  GROUP by day,ticker ORDER by day;
+explain select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2013  GROUP by day,ticker ORDER by day;
+select day,ticker,max(lastPrice) as High ,min(lastPrice) as Low  from   hitsparquet WHERE ticker="IBM" AND lastPrice IS NOT NULL AND YEAR=2013  GROUP by day,ticker ORDER by day;
 
 -- Inner Join 1 month
 explain select distinct a.day FROM hitsparquet a INNER JOIN hitsparquet b USING (day) WHERE (a.month = 07 AND a.ticker = 'IBM' AND a.lastprice > 200.5) AND (b.month = 07 AND b.ticker = 'T' AND b.lastprice > 35.8);
